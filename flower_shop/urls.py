4e59a994fac9164ed095bf0f2c35db57c2f6f7a7
Kirill Rudenko <cs1@list.ru>
2023-01-31 23:27:50 +0300

    Add blank start page

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -18,8 +18,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from flower_shop.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name='mainpage'),
 ]
 
 if settings.DEBUG:
