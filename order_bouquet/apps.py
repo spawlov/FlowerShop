be94259bc52b4b904cbdddcd8a7719b0f294f2ea
AirGuy-one <airguy-one@yandex.ru>
2023-02-02 13:13:09 +0300

    Setup admin panel

--- order_bouquet/apps.py
+++ order_bouquet/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class OrderBouquetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'order_bouquet'
+    verbose_name = 'заказ букета'
