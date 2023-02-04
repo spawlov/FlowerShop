314ea49ff14f03c4d7788a9d9acc58b16072ab97
Kirill <cs1@list.ru>
2023-02-05 01:31:54 +0300

    Add create request for consultation with an operator

--- order_bouquet/views.py
+++ order_bouquet/views.py
@@ -1,7 +1,11 @@
 from decimal import Decimal
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 
-from django.db.models import Q
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+from order_bouquet.forms import ConsultationForm
 
 from order_bouquet.models import Bouquet, Category, Consultation, Order
 
@@ -11,6 +15,13 @@ class HomeView(generic.ListView):
     template_name = 'index.html'
     context_object_name = 'home'
 
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        fname = request.POST.get('fname')
+        tel = request.POST.get('tel')
+        consultation = Consultation(name=fname, phone=tel)
+        consultation.save()
+        context = {'created': True}
+        return render(request, 'consultation_confirm.html', context)
 
 class CatalogView(generic.ListView):
     queryset = Bouquet.objects.all().order_by('?')[:6]
@@ -63,10 +74,19 @@ class ResultView(generic.ListView):
         return queryset
 
 
-class ConsultationView(generic.ListView):
+class ConsultationView(generic.CreateView):
+    form_class = ConsultationForm
     model = Consultation
     template_name = 'consultation.html'
     context_object_name = 'consultations'
+
+    def post(self, request: HttpRequest, *args: str, **kwargs: Any) -> HttpResponse:
+        fname = request.POST.get('fname')
+        tel = request.POST.get('tel')
+        consultation = Consultation(name=fname, phone=tel)
+        consultation.save()
+        context = {'created': True}
+        return render(request, 'consultation_confirm.html', context)
 
 
 class OrderView(generic.ListView):
