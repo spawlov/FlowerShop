660619ada66677ec87566f62f394ecd11b1be38e
spawlov <spawlov@gmail.com>
2023-02-06 17:11:48 +0700

    Added ready function for apps.py

--- order_bouquet/apps.py
+++ order_bouquet/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class OrderBouquetConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'order_bouquet'
+    verbose_name = 'заказ букета'
+
+    def ready(self):
+        from . import signals
